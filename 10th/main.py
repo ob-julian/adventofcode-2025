9f5ad5557ff7af6a727e5c0052bec04d34796b9a
julian <julianoberhofer2@gmail.com>
2025-12-10 14:50:46 +0100

    gen 2 optimised day 10

--- 10th/main.py
+++ 10th/main.py
@@ -8,15 +8,6 @@ def file_reader(file_path):
     with open(os.path.join(os.path.dirname(__file__), file_path), 'r') as f:
         return [[list(int(x) for  x in line[0][1:-1].replace("#", "1").replace(".","0")), [list(int(y) for y in x[1:-1].split(",")) for x in line[1:-1]], [int(z) for z in line[-1][1:-1].split(",")]] for line in [line.split(" ") for line in f.read().splitlines()]] # 100 char line limit, thats why its just a best practice nowadays xD
 
-def generator(length):# deprecated, use generator_optimised
-    iteration = 1
-    while True:
-        result = bin(iteration)[2:].zfill(length)
-        if len(result) > length:
-            return
-        yield result
-        iteration += 1
-
 def generator_optimised(n): # Thx AI, my gen function worked but was inefficient in the order it produced the needed results. With this I could shortcircute the evaluation at fist hit, shortening exec time from 1s to 0.1s
     for r in range(1, n+1):  # number of 1s in the array
         for ones_pos in itertools.combinations(range(n), r):
@@ -46,49 +37,45 @@ def solver1(puzzle_input):
                 break
     return total
 
-def get_sollution(buttons, result):
-    prob = LpProblem("IntegerSolution", LpMinimize)
-
-    # Generate integer variables
-    x = [LpVariable(f"x{i}", lowBound=0, cat=LpInteger) for i in range(len(buttons))]
-
-    # Objective: minimize sum of variables
-    prob += lpSum(x)
-
-    # Constraints
-    actions = [[] for _ in range(len(result))]
-    for i, button in enumerate(buttons):
-        for active_link in button:
-            actions[active_link].append(x[i])
-    for i, line in enumerate(actions):
-        prob += lpSum(line) == result[i]
-
-    prob.solve(PULP_CBC_CMD(msg=False))
-
-    if prob.status != 1:
-        print("-"*50)
-        print("No solution found")
-        print("-"*50)
-        return None  # No solution found
-
-    solution = [value(var) for var in x]
-    return solution
-
 def solver2(puzzle_input):
-    total = 0
+    prob = LpProblem("IntegerSolution", LpMinimize) #its actually faster to create one big problem than multiple small ones, python overhead I guess
+    x = []
+    total_variables = 0
     for machine in puzzle_input:
         end_state = machine[2]
         buttons = machine[1]
-        solution = get_sollution(buttons, end_state)
-        total += sum(solution) if solution is not None else 0
-    return int(total)
 
+        # Generate integer variables
+        x += ([LpVariable(f"x{i+total_variables}", lowBound=0, cat=LpInteger) for i in range(len(buttons))])
+
+        # Constraints
+        actions = [[] for _ in range(len(end_state))]
+        for i, button in enumerate(buttons):
+            for active_link in button:
+                actions[active_link].append(x[i+total_variables])
+        for i, line in enumerate(actions):
+            prob += lpSum(line) == end_state[i]
+
+        total_variables += len(buttons)
+
+    # Adding goal, indicated by no right side constant
+    prob += lpSum(x)
+
+    # solve using standard solver but suppress output
+    prob.solve(PULP_CBC_CMD(msg=False))
+
+    if prob.status != 1:
+        print("No solution found")
+        return None  # No solution found
+
+    solution = sum([value(var) for var in x])
+    return int(solution)
 
 if __name__ == "__main__":
     FILE = "input.txt"
     #FILE = "test.txt"
 
     INPUT = file_reader(FILE)
-    print("Advent of Code: Day ")
+    print("Advent of Code: Day 10")
     print("Solution for Part 1:", solver1(INPUT))
     print("Solution for Part 2:", solver2(INPUT))
